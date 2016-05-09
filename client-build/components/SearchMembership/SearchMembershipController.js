d5035b42b69a51ff931cd9903d9bc5c5622311cc
Rod Monk <roderickmonk@gmail.com>
2016-05-08 17:22:37 -0700

    First Commit

--- client-build/components/SearchMembership/SearchMembershipController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// Search Membership Controller
-app.controller ('SearchMembershipCtrl', ['$scope', '$log', 'MemberService', '$window',
-function ($scope, $log, MemberService, $window) {
-		MemberService.getAllMembers (function (err, members) {
-			err ? 	$window.alert (err + ' - please contact the TTC Webmaster') : $scope.members = members;
-		});
-}]);
